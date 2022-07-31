d1b07ba5b9eacdddc65d4f7c03df2fd19fea9ffc
Mayu Shinde <97580421+Mayur0106@users.noreply.github.com>
2022-07-31 20:33:15 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -56,7 +56,7 @@ public class Main {
         int min=0,max=100;
         int numbers =min + check.nextInt(max);
         Games gh=new Games(numbers);
-        gh.get( );
+        gh.get();
 
 
     }
