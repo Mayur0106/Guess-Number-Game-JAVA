aa684d5555185bedcbf420d54961bd5fd305fcc3
Mayu Shinde <97580421+Mayur0106@users.noreply.github.com>
2022-07-31 20:31:16 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,63 @@
+import java.util.Random;
+import java.util.Scanner;
+
+class Games
+{
+    Scanner generator=new Scanner(System.in);
+
+    boolean checker = true;
+
+    int Random_number;
+//    int num;
+    int count =1;
+    String name ;
+    Games(int num)
+    {
+        Random_number=num;
+        System.out.print("Enter player name : ");
+        String  name=generator.nextLine();
+        name=name;
+        System.out.println("--------------- START ---------------");
+
+    }
+   public void get()
+    {
+
+        do{
+            System.out.print("Enter the number : ");
+            int a=generator.nextInt();
+
+
+            if(a<Random_number)
+            {
+                System.out.println("Given number is less than random number \n");
+            }
+            else if(Random_number<a)
+            {
+                System.out.println("Given number is greater than random number  \n");
+            }
+            else {
+
+                System.out.printf(" \n\tCongratulation %s...! \n ",name);
+                System.out.printf("\tGuess the number is %d attempt.",count);
+                checker = false;
+            }
+            count =count+1;
+
+        }
+        while(checker);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Random check =new Random();
+        int min=0,max=100;
+        int numbers =min + check.nextInt(max);
+        Games gh=new Games(numbers);
+        gh.get();
+
+
+    }
+}
